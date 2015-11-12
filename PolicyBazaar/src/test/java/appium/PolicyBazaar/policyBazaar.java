4383cc2b1bfb02e1818a4161e5940938f2347e0a
Aditya <adityv40@in.ibm.com>
2015-11-12 19:44:06 +0530

    Changed help files and pom library

--- PolicyBazaar/src/test/java/appium/PolicyBazaar/policyBazaar.java
+++ PolicyBazaar/src/test/java/appium/PolicyBazaar/policyBazaar.java
@@ -13,7 +13,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
--- PolicyBazaar/src/test/java/pom/HomePage.java
+++ PolicyBazaar/src/test/java/pom/HomePage.java
@@ -1,5 +1,14 @@
 package pom;
 
+import java.util.List;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+
+
 public class HomePage {
+	
+//	Identify elements
+	//@AndroidFindBy(id);
 
 }
