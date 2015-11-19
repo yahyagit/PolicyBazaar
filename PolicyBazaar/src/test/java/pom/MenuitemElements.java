7e8b05788ae22e636f2df8155ef7e6d35b4d7540
Aditya <adityv40@in.ibm.com>
2015-11-19 20:07:15 +0530

    Added elements in MenuitemElements

--- PolicyBazaar/src/test/java/pom/MenuitemElements.java
+++ PolicyBazaar/src/test/java/pom/MenuitemElements.java
@@ -1,5 +1,41 @@
 package pom;
 
-public class MenuitemElements {
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 
+public class MenuitemElements {
+	
+	//Identify elements
+		@AndroidFindBy(id = "com.policybazaar:id/menuUsername")
+		public MobileElement username;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemHome")
+		public MobileElement home ;
+
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemMyPoloicies")
+		public MobileElement MyPoloicies ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemMyDocuments")
+		public MobileElement MyDocuments ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemTrackApplication")
+		public MobileElement TrackApplication ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemMyProfile")
+		public MobileElement MyProfile ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemTwoWheeler")
+		public MobileElement ItemTwoWheeler ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemCarInsurance")
+		public MobileElement CarInsurance ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemTerm")
+		public MobileElement Terminsurance ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemInvestment")
+		public MobileElement InvestmentPlans ;
+		
+		@AndroidFindBy(id = "com.policybazaar:id/menuItemHealth")
+		public MobileElement HealthInsurance ;
 }
